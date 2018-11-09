572275cbe89f98a3d2895bd26a3ba316c2c88aea
cedis81 <ccchom11@gmail.com>
2018-11-09 11:31:07 -0500

    Completed user based apis
    * user can sign in, sign up, change password, sign out.
    * fields hidden as needed based on user interaction
    * after MVP: need to separate change pw failure interaction
    * updated index.scss to include text color for success/failure
    * updated index.html to separate message id

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,33 +2,55 @@ const store = require('../store.js')
 
 const signUpSuccess = function (signUpResponse) {
   $('#message').html('You signed up successfully')
-  // TODO: add/remove classes once HTML has been built
-  // TODO: modify tags/methods as needed once HTML has been built
+  $('#message').addClass('success-message')
+  $('#message').removeClass('error-message')
+  $('#sign-up-form').addClass('hidden')
 }
 
 const signInSuccess = function (signInResponse) {
   store.user = signInResponse.user
   $('#message').html('You signed in successfully')
-  // TODO: add/remove classes once HTML has been built
-  // TODO: modify tags/methods as needed once HTML has been built
+  $('#message').addClass('success-message')
+  $('#message').removeClass('error-message')
+  $('#sign-up-form').addClass('hidden')
+  $('#sign-in-form').addClass('hidden')
+  $('#change-password-form').removeClass('hidden')
+  $('#sign-out-button').removeClass('hidden')
+  $('#game-board').removeClass('hidden')
+  $('#start-new-game').removeClass('hidden')
 }
 
 const changePasswordSuccess = function (changePasswordResponse) {
   $('#message').html('You changed your password successfully.')
-  // TODO: add/remove classes once HTML has been built
-  // TODO: modify tags/methods as needed once HTML has been built
+  $('#message').addClass('success-message')
+  $('#message').removeClass('error-message')
+  $('#sign-up-form').addClass('hidden')
+  $('#sign-in-form').addClass('hidden')
+  $('#change-password-form').removeClass('hidden')
+  $('#sign-out-button').removeClass('hidden')
+  $('#game-board').removeClass('hidden')
+  $('#start-new-game').removeClass('hidden')
 }
 
 const signOutSuccess = function () {
   $('#message').html('You signed out successfully.')
-  // TODO: add/remove classes once HTML has been built
-  // TODO: modify tags/methods as needed once HTML has been built
+  $('#message').addClass('success-message')
+  $('#message').removeClass('error-message')
+  $('#sign-up-form').removeClass('hidden')
+  $('#sign-in-form').removeClass('hidden')
+  $('#change-password-form').addClass('hidden')
+  $('#sign-out-button').addClass('hidden')
+  $('#game-board').addClass('hidden')
+  $('#start-new-game').addClass('hidden')
 }
 
 const failure = function (failureResponse) {
   $('#message').html('Something went wrong, please try again')
-  // TODO: add/remove classes once HTML has been built
-  // TODO: modify tags/methods as needed once HTML has been built
+  $('#message').addClass('error-message')
+  $('#message').removeClass('success-message')
+  $('#sign-up-form').removeClass('hidden')
+  $('#sign-in-form').removeClass('hidden')
+  $('#change-password-form').removeClass('hidden')
 }
 
 module.exports = {
